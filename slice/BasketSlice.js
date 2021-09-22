19c03a530331880a4ff26bf2c512c8828a4f5dda
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-09-22 09:49:56 +0100

    fix: correct the typo in the word providers in ShoppingCartIcon.js

--- slice/BasketSlice.js
+++ slice/BasketSlice.js
@@ -1,24 +1,24 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
-    items: [],
-},
-
+  items: [],
+};
 
 export const basketSlice = createSlice({
-
-    name: 'basket',
-    initialState,
-    reducer: {
-        addToBasket: (state, action) =>{},
-        removeFromBasket: (state, action) => {},
+  name: 'basket',
+  initialState,
+  reducers: {
+    addToBasket: (state, action) => {
+      state.items = [...state.items, action.payload];
     },
+    removeFromBasket: (state, action) => {},
+  },
 });
 
-export const {addToBasket, removeFromBasket}= basketSlice.actions;
- 
+export const {addToBasket, removeFromBasket} = basketSlice.actions;
+
 // Selectors - this is how we pull information from the global store slice
 
-export const selectItems = (state) => state.basket.items;
+export const selectItems = state => state.basket.items;
 
 export default basketSlice.reducer;

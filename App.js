ade2d724c647f6c9a4fe810812d851d327be9453
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-09-22 09:44:57 +0100

    refactor: cover the whole component inside provider as well as store.getState in App.js

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Providers from './navigation';
+import {Provider as StoreProvider} from 'react-redux';
+import {store} from './store/store';
+//import {createStore} from 'redux';
+//const store = createStore();
+// const App = () => {
+//   return <Providers />;
+// };
 
-const App = () => {
-  return <Providers />;
-};
-
-export default App;
+// export default App;
+export default function App() {
+  //console.log(store.getState());
+  return (
+    <StoreProvider store={store}>
+      <Providers />
+    </StoreProvider>
+  );
+}

8b8a7fc33cd68dc6bfb6ef74d736474798e3e841
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-09-23 09:14:18 +0100

    refactor: dfine navigation route checkout to checkoutProduct.js

--- screens/ShoppingCartIcon.js
+++ screens/ShoppingCartIcon.js
@@ -1,34 +1,39 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, StyleSheet, Platform} from 'react-native';
+import {View, Text, StyleSheet, Platform, TouchableOpacity} from 'react-native';
 import {withNavigation} from 'react-navigation';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import {useSelector} from 'react-redux';
 import {selectItems} from '../slice/BasketSlice';
-
+import {useNavigation} from '@react-navigation/native';
 const ShoppingCartIcon = () => {
   const items = useSelector(selectItems);
+    const navigation = useNavigation();
   return (
-    <View style={{padding: 5}}>
-      <View
-        style={{
-          position: 'absolute',
-          height: 24,
-          width: 24,
-          borderRadius: 15,
-          backgroundColor: 'rgba(95,19,90,0.4)',
-          right: -7,
-          bottom: -3,
-          alignItems: 'center',
-          justifyContent: 'center',
-          zIndex: 2000,
-          marginLeft: 15,
-        }}>
-        <Text style={{color: 'white', fontWeight: 'bold'}}>{items.length}</Text>
+    <TouchableOpacity onPress={() => navigation.navigate('checkout')}>
+      <View style={{padding: 5}}>
+        <View
+          style={{
+            position: 'absolute',
+            height: 24,
+            width: 24,
+            borderRadius: 15,
+            backgroundColor: 'rgba(95,19,90,0.4)',
+            right: -7,
+            bottom: -3,
+            alignItems: 'center',
+            justifyContent: 'center',
+            zIndex: 2000,
+            marginLeft: 15,
+          }}>
+          <Text style={{color: 'white', fontWeight: 'bold'}}>
+            {items.length}
+          </Text>
+        </View>
+        <Icon name="ios-cart" size={30} color="rgb(27,25,80)" />
       </View>
-      <Icon name="ios-cart" size={30} color="rgb(27,25,80)" />
-    </View>
+    </TouchableOpacity>
   );
 };
 export default ShoppingCartIcon;
